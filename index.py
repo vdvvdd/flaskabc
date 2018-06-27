cd468e6fcede9a303ac65a3a56817ee0897f5853
戴浩 <daihao@staff.weibo.com>
2018-06-27 14:09:47 +0800

    u

--- index.py
+++ index.py
@@ -1,13 +1,18 @@
 from flask import Flask 
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', method=['GET', 'POST'])
 def index():
-    msg = 'backend message'
-    return render_template('index.html', message=msg)
+    #msg = 'backend message'
+    if request.method = 'POST':
+        a = request.form['add1']
+        b = request.form['add2']
+        return render_template('index.html', message=a)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='0.0.0.0')
