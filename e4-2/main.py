c7c2247d9dee76ae4e895ce092e13653a0d06e1d
戴浩 <daihao@staff.weibo.com>
2018-06-30 10:03:01 +0800

    u

---
+++ e4-2/main.py
@@ -0,0 +1,23 @@
+from flask import Flask 
+from flask import request 
+from flask import render_template 
+from flask import redirect
+
+app = Flask(__name__)
+
+@app.route("/user", methods=['GET', 'POST'])
+def login():
+    if request.method=='POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if username=='daihao' and password=='123456':
+            return redirect("http://google.com")
+        else:
+            message = "login failed"
+            return render_template('index.html')
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=8080, host='0.0.0.0')
