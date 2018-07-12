5506068d9779a0b9db37d53215949c4b9d19c222
戴浩 <daihao@staff.weibo.com>
2018-07-12 14:12:51 +0800

    u

--- db.py
+++ db.py
@@ -1,30 +1,42 @@
 import pymysql
 
-
-name = input()
 # connect to db
 # docker: 172.17.0.1
 conn = pymysql.connect("172.17.0.1", "daihao", "123456", "flask")
 
 # cursor
 cursor = conn.cursor()
-# sql
-sql = "select * from test"
-sql_ins = "insert into test (name) values ('%s')" %(name)
-
-try:
-    cursor.execute(sql_ins)
-    conn.commit()
-except:
-    conn.rollback()
-
-# execute
-try:
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    print(results)
-except:
-    print("Error: unable to fetch data")
 
 
-conn.close()
+def add_user(username, passowrd):
+    sql = "insert into users (username, password) values ('%s', '%s')" %(username, password)
+
+    try:
+        cursor.execute(sql)
+        conn.commit()
+    except:
+        print("insert error")
+        conn.rollback()
+
+    conn.close()
+
+# # sql
+# sql = "select * from test"
+# sql_ins = "insert into test (name) values ('%s')" %(name)
+
+# try:
+#     cursor.execute(sql_ins)
+#     conn.commit()
+# except:
+#     conn.rollback()
+
+# # execute
+# try:
+#     cursor.execute(sql)
+#     results = cursor.fetchall()
+#     print(results)
+# except:
+#     print("Error: unable to fetch data")
+
+
+# conn.close()
