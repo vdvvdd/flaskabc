0b71ed55ce2f13d065b8a609a09157521b86a9ee
戴浩 <daihao@staff.weibo.com>
2018-07-15 09:53:44 +0800

    u

--- app/routes.py
+++ app/routes.py
@@ -22,7 +22,7 @@ def index():
 def login():
     form = LoginForm()
 
-    if form.validate_on_susbmit():
+    if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title = 'Sign In', form=form)
