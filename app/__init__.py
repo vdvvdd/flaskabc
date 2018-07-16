b6764334f8009778feca04d7f686b6476c80077c
戴浩 <daihao@staff.weibo.com>
2018-07-16 11:34:27 +0800

    u

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import pymysql
 
 app = Flask(__name__)
 app.config.from_object(Config)
+pymysql.install_as_MySQLdb()
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
